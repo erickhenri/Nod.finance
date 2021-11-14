ad55c518b43afb6abf6bd0df81ca8bb02f465d0c
Érick Henrique De Souza Ferreira <87100558+nodlic@users.noreply.github.com>
2021-11-14 11:41:37 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -95,7 +95,7 @@ const DOM = {
             <td class="${CSSclass}">${amount}</td>
             <td class="date">${transaction.date}</td>
             <td>    
-                <img onclick="Transaction.remove(${index})"src="/assets/minus.svg" alt="Remover Transação">
+                <img onclick="Transaction.remove(${index})"src="./assets/minus.svg" alt="Remover Transação">
             </td>
         `
 
